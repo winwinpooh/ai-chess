0b1b984ce62073c823f41907a847f4046f023807
winwinpooh <winniefaustinelie4@gmail.com>
2023-06-20 01:54:57 +0800

    alpha beta

--- src/policy/alpha_beta.hpp
+++ src/policy/alpha_beta.hpp
@@ -6,7 +6,7 @@
  * @brief Policy class for random policy, 
  * your policy class should have get_move method
  */
-class random2{
+class alpha_beta{
 public:
   static Move get_move(State *state, int depth);
   
